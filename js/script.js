6fa3ae1f6e42ae7f5c848f1b913e41747b6f5e06
Przemysław Kowalczyk <przemek.t.kowalczyk@gmail.com>
2020-09-08 23:17:36 +0200

    Modiefie playGame function, by adding const to it.

--- js/script.js
+++ js/script.js
@@ -1,30 +1,34 @@
-function playGame(playerInput) {
-	clearMessages();
+{
 
-	/*ComputerMove*/	
-	let randomNumber = Math.floor(Math.random() * 3 + 1);
-	console.log('Komputer wylosował liczbę ' + randomNumber);
+	const playGame = function (playerInput) {
+		clearMessages();
 
-	let computerMove = getMoveName(randomNumber);
-	console.log('Liczba ' + randomNumber + ' to '  + computerMove);
+		/*ComputerMove*/	
+		let randomNumber = Math.floor(Math.random() * 3 + 1);
+		console.log('Komputer wylosował liczbę ' + randomNumber);
 
-	/*PlayerMove*/
-	let playerMove = getMoveName(playerInput);
-	console.log('Gracz wpisał liczbę: ' + playerInput);
-	console.log('Liczba ' + playerInput + ' to ' + playerMove);
+		let computerMove = getMoveName(randomNumber);
+		console.log('Liczba ' + randomNumber + ' to '  + computerMove);
 
-	/*Game score*/
-	console.log('Komputer - ' + computerMove + ', ' + 'Gracz - ' + playerMove);
-	displayResult(computerMove, playerMove);
-}
+		/*PlayerMove*/
+		let playerMove = getMoveName(playerInput);
+		console.log('Gracz wpisał liczbę: ' + playerInput);
+		console.log('Liczba ' + playerInput + ' to ' + playerMove);
+
+		/*Game score*/
+		console.log('Komputer - ' + computerMove + ', ' + 'Gracz - ' + playerMove);
+		displayResult(computerMove, playerMove);
+	}
 
 
-document.getElementById('play-rock').addEventListener('click', function(){
-	playGame(1);
-  });
-document.getElementById('play-paper').addEventListener('click', function(){
-	playGame(2);
-  });
-document.getElementById('play-scissors').addEventListener('click', function(){
-	playGame(3);
-  });
+	document.getElementById('play-rock').addEventListener('click', function(){
+		playGame(1);
+	});
+	document.getElementById('play-paper').addEventListener('click', function(){
+		playGame(2);
+	});
+	document.getElementById('play-scissors').addEventListener('click', function(){
+		playGame(3);
+	});
+
+} 
